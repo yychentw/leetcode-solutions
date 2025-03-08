fc2be7156ecaa60523ada5caaaedf770dd5a3858
yychentw <yingyuchentw003@gmail.com>
2025-03-08 17:05:05 +0800

    Update the C++ solution source code

--- 875. Koko Eating Bananas/solution.cpp
+++ 875. Koko Eating Bananas/solution.cpp
@@ -1,6 +1,32 @@
+#include <vector>
+using namespace std;
+
 class Solution {
-public:
-    int exampleSolution() {
-        return 0;
-    }
-};
+    public:
+        int minEatingSpeed(vector<int>& piles, int h) {
+            int max_num_bananas = 0;
+            for (int num: piles) {
+                if (num > max_num_bananas) {
+                    max_num_bananas = num;
+                }
+            }
+    
+            int left = 1;
+            int right = max_num_bananas;
+            while (left <= right) {
+                int mid  = left + (right-left)/2;
+                long required_h = 0;
+                for (int num: piles) {
+                    required_h += ceil((double)num/mid);
+                }
+    
+                if (required_h > h) {
+                    left = mid + 1;
+                }
+                else if (required_h <= h) {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    };
