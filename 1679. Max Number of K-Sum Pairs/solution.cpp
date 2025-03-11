cd456417393c927d2e646dd56a01a8642dccbf2b
yychentw <yingyuchentw003@gmail.com>
2025-03-11 22:35:23 +0800

    Re-format the C++ implementation

--- 1679. Max Number of K-Sum Pairs/solution.cpp
+++ 1679. Max Number of K-Sum Pairs/solution.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 class Solution {
-    public:
-        int maxOperations(vector<int>& nums, int k) {
-            unordered_map<int, int> umap;
-            int max_ops = 0;
-            for (int n: nums) {
-                int c = k - n;
-                if (umap[c] > 0) {
-                    max_ops++;
-                    umap[c]--;
-                    continue;
-                }
-                umap[n]++;
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> umap;
+        int max_ops = 0;
+        for (int n: nums) {
+            int c = k - n;
+            if (umap[c] > 0) {
+                max_ops++;
+                umap[c]--;
+                continue;
             }
-            return max_ops;
+            umap[n]++;
         }
-    };
+        return max_ops;
+    }
+};
